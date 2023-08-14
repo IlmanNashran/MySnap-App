6e0bae2d5c193cce2cea4296a0aa577aea1c0da5
IlmanNashran <ilmannashran@gmail.com>
2023-08-15 02:43:56 +0800

    create title with button on register page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -8,11 +8,50 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  double? _deviceHeight, _deviceWidth;
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Container(
-        color: Colors.red,
+        padding: EdgeInsets.symmetric(horizontal: _deviceWidth! * 0.05),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _titleWidget(),
+              _registerButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+// create title function
+  Widget _titleWidget() {
+    return const Text(
+      "Snap App",
+      style: TextStyle(
+          color: Colors.black, fontSize: 40, fontWeight: FontWeight.w500),
+    );
+  }
+
+  //register button
+  Widget _registerButton() {
+    return MaterialButton(
+      onPressed: () {},
+      minWidth: _deviceWidth! * 0.50,
+      height: _deviceHeight! * 0.05,
+      color: Colors.red,
+      child: const Text(
+        "Register",
+        style: TextStyle(
+            color: Colors.white, fontSize: 20, fontWeight: FontWeight.w400),
       ),
     );
   }
