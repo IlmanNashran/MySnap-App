83115bb9b93b12bc297f26c47e8b082e728c1221
IlmanNashran <ilmannashran@gmail.com>
2023-08-15 21:46:13 +0800

    create register UI

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -10,6 +10,10 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   double? _deviceHeight, _deviceWidth;
 
+  //From state
+  final GlobalKey<FormState> _registerFormKey = GlobalKey<FormState>();
+  String? _name, _email, _password;
+
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
@@ -24,6 +28,7 @@ class _RegisterPageState extends State<RegisterPage> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               _titleWidget(),
+              _registrationForm(),
               _registerButton(),
             ],
           ),
@@ -53,6 +58,69 @@ class _RegisterPageState extends State<RegisterPage> {
         style: TextStyle(
             color: Colors.white, fontSize: 20, fontWeight: FontWeight.w400),
       ),
+    );
+  }
+
+  Widget _registrationForm() {
+    return Container(
+      height: _deviceHeight! * 0.30,
+      child: Form(
+        key: _registerFormKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _nameTextField(),
+            _emailTextField(),
+            _passwordTextField(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _nameTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(hintText: "Name..."),
+      validator: (_value) =>
+          _value!.length > 0 ? null : "Please enter your name",
+      onSaved: (_value) {
+        setState(() {
+          _name = _value;
+        });
+      },
+    );
+  }
+
+  Widget _emailTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(hintText: "Email...."),
+      onSaved: (_value) {
+        setState(() {
+          _email = _value;
+        });
+      },
+      validator: (_value) {
+        bool _result = _value!.contains(
+          RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$'),
+        );
+        return _result ? null : "Please enter a valid email";
+      },
+    );
+  }
+
+  Widget _passwordTextField() {
+    return TextFormField(
+      obscureText: true, //unseen the password
+      decoration: const InputDecoration(hintText: "Password...."),
+      onSaved: (_value) {
+        setState(() {
+          _email = _value;
+        });
+      },
+      validator: (_value) =>
+          _value!.length > 6 ? null : "Please enter a valid password",
     );
   }
 }
