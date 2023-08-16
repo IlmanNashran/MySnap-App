fead66d1fe483105417163af3a8548b97bbf6900
IlmanNashran <ilmannashran@gmail.com>
2023-08-16 08:44:06 +0800

    create appbar home page title

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text("Snapp App"),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Icon(Icons.add_a_photo),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 8.0,
+              right: 8.0,
+            ),
+            child: GestureDetector(
+              onTap: () {},
+              child: Icon(Icons.logout),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
