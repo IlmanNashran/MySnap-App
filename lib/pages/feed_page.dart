390519827b336c6a351d0f87898af4c183e50875
IlmanNashran <ilmannashran@gmail.com>
2023-08-18 21:42:58 +0800

    adding postview in feed page and in firebase service

--- lib/pages/feed_page.dart
+++ lib/pages/feed_page.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mysnap_app/pages/services/firebase_service.dart';
 
 class FeedPage extends StatefulWidget {
   @override
@@ -10,10 +13,62 @@ class FeedPage extends StatefulWidget {
 //-------------------------------
 
 class _FeedPageState extends State<FeedPage> {
+  double? _deviceHeight, _deviceWidth;
+
+  FirebaseService? _firebaseService;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _firebaseService = GetIt.instance.get<FirebaseService>();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
     return Container(
-      color: Color.fromARGB(255, 231, 20, 157),
+      height: _deviceHeight!,
+      width: _deviceWidth!,
+      child: _postListView(),
+    );
+  }
+
+  Widget _postListView() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firebaseService!.getLatestPosts(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          List _posts = snapshot.data!.docs
+              .map((e) => e.data())
+              .toList(); //get snapshot into _posts
+          return ListView.builder(
+            itemCount: _posts.length,
+            itemBuilder: (BuildContext context, int index) {
+              Map _post = _posts[index];
+              return Container(
+                height: _deviceHeight! * 0.30,
+                margin: EdgeInsets.symmetric(
+                    vertical: _deviceHeight! * 0.01,
+                    horizontal: _deviceWidth! * 0.05),
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                          _post["image"],
+                        ))),
+              );
+            },
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        ;
+      },
     );
   }
 }
